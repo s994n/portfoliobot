472731ffaa0f04049c8f2c66bcfad8060583d816
SimonN <65023744+s994n@users.noreply.github.com>
2023-09-28 14:04:39 +0100

    feat: Add rate limit

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import openai
 from flask import Flask, request, jsonify
 import os
 from background_info import script
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+print('hello')
 
 # Load .env file
 load_dotenv()
@@ -13,6 +16,12 @@ app = Flask(__name__)
 # Load OpenAI API key from environment variable
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# limiter = Limiter(app, key_func=get_remote_address, default_limits=["30 per day", "10 per hour"])
+
+def get_forwarded_for():
+    return request.headers.get('X-Forwarded-For', request.remote_addr)
+
+limiter = Limiter(app, key_func=get_forwarded_for, default_limits=["30 per day", "10 per hour"])
 
 @app.route("/")
 def hello():
@@ -20,6 +29,7 @@ def hello():
 
 
 @app.route("/ask", methods=["POST"])
+@limiter.limit("10 per hour")
 def ask():
     prompt = request.json.get("prompt")
 
